547cb1d718c239195a40ea98d62794e18912301d
Davide Calza <Davide.Calza@marconi.loc>
2017-11-30 12:53:00 +0100

    Working webservice

--- script/webservice/index.js
+++ script/webservice/index.js
@@ -1,26 +1,19 @@
-var cors = require('cors') //enable cross domain calls
+var cors = require('cors')
 var express = require('express');
-var compression = require('compression'); // enable/disable compression
+var compression = require('compression'); 		
 var config = require('./local_conf.js');		
-
 var app = express();
-
 app.use(cors()) 								
 app.use(compression());      					
-app.use(express.static(__dirname + '/public')); 
-
-var alluser = require('./alluser.js');			// Tutti servizi pubblici vengono dirottati 
-app.use('/', alluser);							// a "alluser.js"
+var queries = require('./queries.js');			
+app.use('/', queries);			
 
 var port = config.ws_port;
 
 var server = app.listen(port, function () {
     console.log("")
-    console.log("*******************************************************************************************");
-    console.log("Fototeca Museo della Guerra - REST WS listening at http://localhost:%s", port);
-    console.log("*******************************************************************************************");
+    console.log("");
     console.log("Database: " + config.connection.database);
-    console.log("Directory: " + __dirname);
     console.log("Start at:  " + Date());
-    console.log("*******************************************************************************************")
+    console.log(">")
 });
---
+++ script/webservice/queries.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var config = require('./local_conf.js');
+
+/*************************************************************/
+
+router.get('/Aircrafts', function (req, res) {
+    eseguiQuery(res, 'SELECT * FROM aircraft ORDER BY id;');
+});
+
+router.get('/Tracks', function (req, res) {
+    eseguiQuery(res, 'SELECT * FROM tracks ORDER BY id;');
+});
+
+router.get('/Version', function (req, res) {
+    res.end(config.ws_version);
+})
+
+router.get('/Aircrafts/:id', function (req, res) {
+    eseguiQuery(res, 'SELECT * FROM aircraft WHERE id=?;', req.params.id);
+});
+
+/*************************************************************/
+
+function eseguiQuery(res, sQuery, aParam) {
+
+    console.log('> Request: SQL -- ' + sQuery); //debug
+
+    connection = mysql.createConnection(config.connection);
+    connection.connect();
+    connection.query(sQuery, aParam, function (err, rows, fields) {
+        if (!err) {
+            res.json(rows);
+        }
+        else {
+            console.log('> ERROR: ' + err)
+            res.json('{}');
+        }
+    });
+    connection.end();
+}
+
+module.exports = router;
