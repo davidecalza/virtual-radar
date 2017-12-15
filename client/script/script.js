b6bb5f99559ec2429294571e100facb173edfc49
Davide.Calza <davide.calza@marconirovereto.it>
2017-12-15 16:13:23 +0100

    3_Bug fix real time speed chart update

--- client/script/script.js
+++ client/script/script.js
@@ -275,13 +275,14 @@ function loadPlanes() {
                             "time": time,
                             "speed": aircraft.speed
                         };
-                        if(d.getMinutes() > JSON.stringify(speed_tracking[x].data[speed_tracking[x].data.length-1].time).split(':')[1]){
+                        if(d.getMinutes().toString() > JSON.stringify(speed_tracking[x].data[speed_tracking[x].data.length-1].time).split(':')[1]){
                             speed_tracking[x].data.push(objData);
                             exists = true;
                         }
-                        else if(d.getMinutes() == JSON.stringify(speed_tracking[x].data[speed_tracking[x].data.length-1].time).split(':')[1]
-                                && d.getSeconds() > JSON.stringify(speed_tracking[x].data[speed_tracking[x].data.length-1].time).split(':')[2].split('"')[0])
+                        else if(d.getMinutes().toString() === JSON.stringify(speed_tracking[x].data[speed_tracking[x].data.length-1].time).split(':')[1]
+                                && d.getSeconds().toString() > JSON.stringify(speed_tracking[x].data[speed_tracking[x].data.length-1].time).split(':')[2].split('"')[0])
                         {
+                            //alert('seconds');
                             speed_tracking[x].data.push(objData);
                             exists = true;
                         }
